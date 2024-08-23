42c882dfc7f741df2255a8d3197d2c7b08adb2d9
Rodrigo Ximenes <rodrigogaldino@poli.ufrj.br>
2024-08-22 22:46:03 -0300

    2º commit

--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse('Olá mundo');
+    return render(request, "todos/home.html")
